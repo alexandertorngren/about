c393b114c991968b3e280de8f58e7f97d92f6a3d
Alexander Torngren <alexander.torngren@drakeanalytics.se>
2022-05-07 15:31:40 +0200

    Default export

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,10 @@ import { useEffect, useState } from 'react';
 import '../styles/bootstrap.css';
 import '../styles/global.css';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   const [layout, setLayout] = useState(null);
   useEffect(() => setLayout(document), []);
 
   layout ? (layout.getElementById('__next').className = 'h-100') : null;
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
