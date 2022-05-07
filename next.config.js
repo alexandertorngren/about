50df53bf5ef6c2fca984b439ad06c4562b5ca68a
Alexander Torngren <alexander.torngren@drakeanalytics.se>
2022-05-07 15:28:36 +0200

    Fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
   assetPrefix: isProd ? '/about/' : '',
 };
-
-module.exports = nextConfig;
